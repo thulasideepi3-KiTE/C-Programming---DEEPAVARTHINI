1021a1e10b6f6e7c83b57dd6afb9b2da30cd0f39
thulasideepi3-KiTE <thulasideepi3@gmail.com>
2025-08-12 13:04:56 +0530

    Eleventh Code

---
+++ ipadd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int c;
+    printf("Enter the number A: ");
+    scanf ("%d",&a);
+    printf("Enter the number B: ");
+    scanf ("%d",&b);
+    c= a+b;
+    printf("The added numbers are %d",c);
+    return 0;
+}
