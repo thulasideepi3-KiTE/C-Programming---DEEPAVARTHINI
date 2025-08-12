8f6d64358b7c3ac9b7a0125a8599d7fc60e71062
thulasideepi3-KiTE <thulasideepi3@gmail.com>
2025-08-12 12:39:57 +0530

    Fifth and sixth Program

---
+++ line.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf(" Hello World! \n \n C Programming");
+    return 0;
+}
---
+++ specialcharacters.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("@\n");
+    printf("#\n");
+    printf("$\n");
+    printf("%%\n"); // %% is used to print a single %
+    return 0;
+}
