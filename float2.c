c54a769ca8ebbb1b62640d08dc12e8162562c02c
thulasideepi3-KiTE <thulasideepi3@gmail.com>
2025-08-13 08:37:18 +0530

    Seventeenth Code

---
+++ float2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float a , b ;
+    printf("Enter the float value A: ");
+    scanf("%f", &a);
+     printf("Enter the float value B: ");
+    scanf("%f", &b);
+    float c = a+b;
+    printf("%.2f",c);
+    return 0;
+
+}
