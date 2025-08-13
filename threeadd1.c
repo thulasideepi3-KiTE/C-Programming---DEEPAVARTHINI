9dd04520715a31f3c37e709dddf032d45b9091cd
thulasideepi3-KiTE <thulasideepi3@gmail.com>
2025-08-13 08:24:13 +0530

    Fifteenth code

---
+++ threeadd1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c ;
+    printf("Enter the value of A: ");
+    scanf("%d",&a);
+    printf("Enter the value of B: ");
+    scanf("%d",&b);
+    printf("Enter the value of C: ");
+    scanf("%d",&c);
+    int d = a +b +c;
+    printf("The sum is: ");
+    printf("%d", d);
+}
