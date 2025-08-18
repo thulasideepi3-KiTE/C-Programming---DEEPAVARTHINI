13395364f4fedd6b8ff4b98e7643e020bb46b573
thulasideepi3-KiTE <thulasideepi3@gmail.com>
2025-08-18 08:30:38 +0530

    23th code

---
+++ greater.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a ;
+    int b;
+    printf("Enter the Two Digit Value: ");
+    scanf("%d",&a);
+     printf("Enter the One Digit Value: ");
+    scanf("%d",&b);
+    int c = a-b;
+    printf("The value is %d",c);
+}
