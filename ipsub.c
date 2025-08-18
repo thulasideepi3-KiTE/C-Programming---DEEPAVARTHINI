c6196d2d04bd921393cb59cec9975b1e629048fe
thulasideepi3-KiTE <thulasideepi3@gmail.com>
2025-08-18 08:27:52 +0530

    22th Code

---
+++ ipsub.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a ;
+    int b;
+    printf("Enter the A Value: ");
+    scanf("%d",&a);
+     printf("Enter the B Value: ");
+    scanf("%d",&b);
+    int c = a-b;
+    printf("The value is %d",c);
+}
