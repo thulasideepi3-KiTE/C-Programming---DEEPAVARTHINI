6dc08269f2bc12f28ca3810ea7a384e5862c4250
thulasideepi3-KiTE <thulasideepi3@gmail.com>
2025-08-18 08:11:53 +0530

    19th Code

---
+++ addvar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a , b ;
+    printf("Enter the float value A: ");
+    scanf("%d", &a);
+     printf("Enter the float value B: ");
+    scanf("%d", &b);
+    int c = a+b;
+    printf("Sum = %d",c);
+    return 0;
+
+}
